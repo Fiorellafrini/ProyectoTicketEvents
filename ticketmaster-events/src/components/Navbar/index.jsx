83c3dc472f020481e4fd2a01bfb5509a6744c1d2
fiorella frini <fiorella_frini@hotmail.com>
2023-09-05 12:08:46 -0300

    search

--- ticketmaster-events/src/components/Navbar/index.jsx
+++ ticketmaster-events/src/components/Navbar/index.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+
+  const handleInputChange = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <div>
       <p>Mi boletera</p>
-      <input placeholder="Busca tu evento favorito" />
+      <input
+        placeholder="Busca tu evento favorito"
+        onChange={handleInputChange}
+        value={search}
+      />
     </div>
   );
 };
